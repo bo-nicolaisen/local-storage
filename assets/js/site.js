da7004a183e1eedbdd90d50582a3791333168c43
bo-nicolaisen <bni@techcollege.dk>
2024-08-27 08:48:45 +0200

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,25 @@
 // write cool JS hwere!!
+
+
+localStorage.setItem('data', 'hund')
+localStorage.setItem('ib', 'kat')
+
+console.log(localStorage.getItem('bob'));
+
+
+localStorage.removeItem("ib");
+
+
+function SaveObject(basketData, itemName) {
+    let mySerializedData = JSON.stringify(basketData)
+    localStorage.setItem(itemName, mySerializedData)
+}
+
+
+
+function ReadObject(itemName) {
+    let mybasketstring = localStorage.getItem(itemName)
+
+    let myBasket = JSON.parse(mybasketstring)
+    return myBasket
+}
